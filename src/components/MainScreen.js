a99c7c09ad834f303b1949192021a73a5b61b4d4
dimplehh <dimplehh@naver.com>
2021-08-11 22:04:59 +0900

    사진확인

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -6,21 +6,43 @@ import {
   StyleSheet,
   Button,
   Text,
+  Image
 } from 'react-native';
 import {RectButton} from 'react-native-gesture-handler';
 import SwipeableViews from 'react-swipeable-views-native';
 import {RNCamera} from 'react-native-camera';
 import PendingView from './PendingView';
 import Tts from 'react-native-tts';
+import TesseractOcr, {LANG_KOREAN, useEventListener} from 'react-native-tesseract-ocr';
+
+const DEFAULT_HEIGHT=500;
+const DEFAULT_WIDTH=600;
 
 class MainScreen extends React.Component {
   state = {
     pausePreview: false,
     image: 'none', // 유통기한 이미지 주소.
   };
+  //const [text, setText] = useState('');
 
-  returnData = image => {
+  returnData = async(image) => {
     this.setState({image: image});
+    console.log('image:',image);
+    // try {
+    //     //const tesseractOptions = {};
+    //     var recognizedText = TesseractOcr.recognize(image,LANG_KOREAN,tesseractOptions);
+    //     //console.log('나는 어떻게나올까',recognizedText);
+    //     //if recognizedText 에 유통기한이라는 글자 있으면 그 뒤에 말을 setText로 삼고 그것을 변수로 두고 읽기
+    //     // if (recognizedText.includes('짧다')){
+    //     //   recognizedText = "유통기한 찾았다.";
+    //     //   setText(recognizedText);
+    //     // }
+    //     //setText(recognizedText);
+    //     //Speech.speak(recognizedText, {language : 'ko'});
+    //   } catch (err) {
+    //     console.error(err);
+    //     setText('');
+    //   }
   };
 
   handlePressPicture = () => {
@@ -63,14 +85,17 @@ class MainScreen extends React.Component {
         <View
           style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
           value={0}>
-          <Pressable onPress={this.handlePressPicture} style={styles.btn}>
-            <Text style={styles.txt}>{image}</Text>
-          </Pressable>
+              {image && (
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image} source={{uri:image}} />
+                        <Text>{image}</Text>
+                    </View>
+                )}
         </View>
         <View
           style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
           value={1}>
-          <Text style={styles.txt}>Uhear이에요.</Text>
+          <Text style={styles.txt}>Uhear이에요</Text>
         </View>
         <View
           style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
@@ -124,6 +149,15 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: 'blue',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginVertical: 15,
+    height: DEFAULT_HEIGHT / 2.5,
+    width: DEFAULT_WIDTH / 2.5,
   },
 });
 
